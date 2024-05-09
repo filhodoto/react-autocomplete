219c8bf43a2eb9d5828d2466b70a8b39d9e4f355
Gonçalo Ramalho <goncalo_ramalho@hotmail.com>
2024-05-09 15:31:59 +0100

    Select option on click

--- src/components/Autocomplete/index.tsx
+++ src/components/Autocomplete/index.tsx
@@ -1,35 +1,58 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
 import './styles.css'; // Import stylesheet
 
-const Autocomplete = ({ placeholder, options }) => {
+interface OptionProps {
+  id: string;
+  name: string;
+}
+
+interface AutocompleteProps {
+  placeholder: string;
+  options: OptionProps[];
+}
+
+const Autocomplete = ({ placeholder, options }: AutocompleteProps) => {
   const [userInput, setUserInput] = useState('');
   const [showNoResults, setShowNoResults] = useState(false);
-  const [suggestions, setSuggestions] = useState(options);
+  const [suggestions, setSuggestions] = useState<OptionProps[]>([]);
 
-  const handleChange = (e) => {
+  const inputSearchRef = useRef<HTMLInputElement>(null);
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setUserInput(e.target.value);
+  };
+
+  const handleSuggestionClick = (suggestionText: string) => {
+    setUserInput(suggestionText);
+
+    // Close list
+    setSuggestions([]);
+
+    // Focus on input after choosing option from list
+    inputSearchRef.current?.focus();
   };
 
   return (
     <div className="autocomplete-container">
       <input
+        ref={inputSearchRef}
         type="text"
         className="autocomplete-input"
         id="autocomplete-input"
         placeholder={placeholder}
         value={userInput}
-        onChange={handleChange}
+        onChange={handleInputChange}
         onFocus={() => setSuggestions(options)}
       />
       {suggestions.length > 0 && (
         <ul className="autocomplete-suggestions">
-          {suggestions.map((suggestion) => (
+          {suggestions.map(({ id, name }) => (
             <li
-              key={suggestion} // Assuming suggestions have unique keys
+              key={id}
               className="autocomplete-suggestion"
-              //   onClick={() => handleSuggestionClick(suggestion)}
+              onClick={() => handleSuggestionClick(name)}
             >
-              {suggestion.name}
+              {name}
             </li>
           ))}
         </ul>
