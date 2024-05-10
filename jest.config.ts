a2d5165f6b17947d770d140436510a8f00981a0f
Gonçalo Ramalho <goncalo_ramalho@hotmail.com>
2024-05-10 15:19:51 +0100

    Fix jest type issue with jest 6

--- jest.config.ts
+++ jest.config.ts
@@ -103,7 +103,6 @@ const config: Config = {
 
   // A preset that is used as a base for Jest's configuration
   preset: 'ts-jest',
-
   // Run tests from one or more projects
   // projects: undefined,
 
@@ -137,7 +136,7 @@ const config: Config = {
   // setupFiles: [],
 
   // A list of paths to modules that run some code to configure or set up the testing framework before each test
-  // setupFilesAfterEnv: [],
+  setupFilesAfterEnv: ['<rootDir>/jest-setup.ts'],
 
   // The number of seconds after which a test is considered as slow and reported as such in the results.
   // slowTestThreshold: 5,
@@ -175,7 +174,10 @@ const config: Config = {
   // testRunner: "jest-circus/runner",
 
   // A map from regular expressions to paths to transformers
-  // transform: undefined,
+  // To process `*.tsx` files with `ts-jest`
+  transform: {
+    '^.+\\.tsx?$': 'ts-jest',
+  },
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
